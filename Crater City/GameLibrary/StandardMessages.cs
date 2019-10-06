5635d12ef4a1d894c5b7b9d2bd01126e3277378e
colemank0259 <43657783+colemank0259@users.noreply.github.com>
2019-10-06 09:43:43 -0400

    Updated StandardMessages
    Added methods to StandardMessages

--- Crater City/GameLibrary/StandardMessages.cs
+++ Crater City/GameLibrary/StandardMessages.cs
@@ -8,5 +8,41 @@ namespace GameLibrary
 {
     public class StandardMessages
     {
+        public static void DisplayOpeningMenu()
+        {
+            Console.Write("Enter 1 or 2 to select: ");
+            Console.Write("");
+            Console.WriteLine("1. Run Program");
+            Console.WriteLine("2. Exit");
+        }
+
+        public static void DisplayOpeningMenuError()
+        {
+            Console.WriteLine("You must enter 1 or 2.");
+        }
+
+        public static void DisplayIntError()
+        {
+            Console.WriteLine("You must enter an integer.");
+        }
+
+        public static void DisplayProgramDescription()
+        {
+            Console.WriteLine("This program stores student infomation and calculates grade averages.");
+        }
+
+        public static void DisplayMainMenu()
+        {
+            Console.Write("Enter 1, 2 or 3 to select: ");
+            Console.WriteLine("");
+            Console.WriteLine("1. Enter Student");
+            Console.WriteLine("2. Get Average");
+            Console.WriteLine("3. Exit");
+        }
+
+        public static void DisplayMainMenuError()
+        {
+            Console.WriteLine("You must enter 1, 2 or 3.");
+        }
     }
 }
